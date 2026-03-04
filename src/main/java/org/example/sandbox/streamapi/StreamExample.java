6567b50f67bee219e229c349113ca12ae361fac5
Spyro1001 <raymondhedgecock@gmail.com>
2026-03-03 19:33:32 -0700

    adding streams

--- src/main/java/org/example/sandbox/streamapi/StreamExample.java
+++ src/main/java/org/example/sandbox/streamapi/StreamExample.java
@@ -2,6 +2,7 @@ package org.example.sandbox.streamapi;
 
 import java.util.*;
 import java.util.function.*;
+import java.util.stream.*;
 
 public class StreamExample {
 
@@ -12,7 +13,63 @@ public class StreamExample {
         IntPredicate evenOnly = x -> x % 2 == 0;
         Predicate<Integer> evenOnlyPredicate = x -> x % 2 == 0;
 
+        // intermediate operations
+        // map
+        List<Integer> square = numbers
+                .stream()
+                .map(x -> x * x) // monad
+                .collect(Collectors.toList());
+        square.forEach(System.out::println);
 
+        // filter
+        List<String> result = names
+                .stream()
+                .filter(s -> s.startsWith("S"))
+                .collect(Collectors.toList());
+        result.forEach(System.out::println);
+
+        // sorted
+        List<String> result2 = names
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
+        result2.forEach(System.out::println);
+
+        // terminal operations
+        // collect
+        Set<Integer> squared = numbers
+                .stream()
+                .map(x -> x * x)
+                .collect(Collectors.toSet());
+        squared.forEach(System.out::println);
+
+        // foreach
+        numbers
+                .stream()
+                .map(x -> x * x)
+                .forEach(System.out::println);
+
+        // reduce
+        int sum = 0;
+        for (int x = 1; x<=5; x++) {
+            if (x % 2 == 0) {
+                sum += x;
+            }
+        }
+        System.out.println(sum);
+
+        int evenSum = numbers.stream()
+                .filter(x -> x % 2 == 0)
+                .reduce(0, (acc, num) -> acc + num);
+        System.out.println(evenSum);
+
+        // int streams
+        int totalSum = IntStream.iterate(0, x -> x + 1)
+                .skip(5)
+                .limit(5)
+                .filter(evenOnly)
+                .reduce(2, Integer::sum);
+        System.out.println(totalSum);
 
     }
 }
